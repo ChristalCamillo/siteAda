91c8fb41774118f1bd2a314feceb37bfb06aae12
christal <christal.b.camillo@gmail.com>
2025-06-26 16:45:19 -0300

    iniciando js, condicionais e getElementById

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// IF nome && email estiverem preenchidos no formulario, POST
+// ELSE peça a usuaria que preencha todos os campos
+// volte a tentar enviar
+
+var nome = document.getElementById("nome")
+var email = document.getElementById("email")
+
+if (nome && email) {
+
+}
