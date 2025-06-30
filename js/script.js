2a54c93e0b83fc176d85ff785c772aed0d12b712
christal <christal.b.camillo@gmail.com>
2025-06-30 12:01:53 -0300

    adicionando mensagem de alert

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
-// IF nome && email estiverem preenchidos no formulario, POST
+// IF nome && email forem diferentes de vazio (preenchidos), POST
 // ELSE peça a usuaria que preencha todos os campos
 // volte a tentar enviar
 
-var nome = document.getElementById("nome")
-var email = document.getElementById("email")
+var nome = document.getElementById("nome").value
+var email = document.getElementById("email").value
 
-if (nome && email) {
+if (nome != "" && email != "") {
+    alert("Tudo certo!")
 
+} else {
+    alert("Preencha nome e email para receber as novidades!")
 }
