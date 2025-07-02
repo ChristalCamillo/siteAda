1c34cf57c5c978bc96d1925f874054763cdbee0f
christal <christal.b.camillo@gmail.com>
2025-07-02 12:22:33 -0300

    site pronto conforme curriculo do curso. não entendi porque meu uso de variaveis deu errado

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
 // IF nome && email forem diferentes de vazio (preenchidos), POST
 // ELSE peça a usuaria que preencha todos os campos
 // volte a tentar enviar
+document.getElementById("botaoEnviar").addEventListener("click", validaFormulario)
 
-var nome = document.getElementById("nome").value
-var email = document.getElementById("email").value
+// nome = document.getElementById("nome").value
+// email = document.getElementById("email").value
+// telefone = document.getElementById("telefone").value
 
-if (nome != "" && email != "") {
-    alert("Tudo certo!")
+function validaFormulario() {
 
-    document.getElementById("botaoEnviar").addEventListener("click",)
-
-} else {
-    alert("Preencha nome e email para receber as novidades!")
+    if (document.getElementById("nome").value != "" && document.getElementById("email").value != "" && document.getElementById("telefone").value != "") {
+        alert("Prontinho! você receberá as novidades por email.")
+    } else {
+        alert("Por favor, preencha os campos nome e email.")
+    }
 }
